bd629e11d3f87d6680d6f19875d871654796904a
Adam Kater <adam.kater@gmail.com>
2018-12-31 12:58:13 -0500

    Sentinel name utilities

--- sentinel_gcp_filter.py
+++ sentinel_gcp_filter.py
@@ -1,10 +1,16 @@
 import gzip
 import collections
+import itertools
 from typing import NamedTuple
 from datetime import datetime
 from google.cloud import storage
+from osgeo import gdal
+from osgeo import ogr
+from osgeo import osr
 
 date_format="%Y-%m-%dT%H:%M:%S.%fZ"
+
+output_file='test_output/test1.gpkg'
 
 storage_client = storage.Client()
 bucket_name='gcp-public-data-sentinel-2'
@@ -30,13 +36,66 @@ class Granule(NamedTuple):
 
 def parseRecord(line):
     tokens=str.splitlines(line)[0].split(',')
-    return Granule(*tokens[0:4], datetime.strptime(tokens[4], date_format), *tokens[5:8],  
-    datetime.strptime(tokens[8], date_format), *tokens[9:])
+    return Granule(
+        *tokens[0:4], 
+        datetime.strptime(tokens[4], date_format), 
+        *tokens[5:8],  
+        datetime.strptime(tokens[8], date_format), 
+        *map(float, tokens[9:13]),
+        tokens[13])
+
+gpkgDriver = ogr.GetDriverByName("GPKG")
+dataset = gpkgDriver.CreateDataSource(output_file)
+srs = osr.SpatialReference()
+srs.ImportFromEPSG(4326)
+layer1 = dataset.CreateLayer('image_bounds', srs, geom_type=ogr.wkbPolygon)
+layer1.CreateField(ogr.FieldDefn("granule_id", ogr.OFTString))
+layer1.CreateField(ogr.FieldDefn( "product_id", ogr.OFTString))
+layer1.CreateField(ogr.FieldDefn( "cloud_cover", ogr.OFTReal))
+layer1.CreateField(ogr.FieldDefn("sensing_date", ogr.OFTDateTime))
+layer1.CreateField(ogr.FieldDefn("mgrs_tile", ogr.OFTString))
+
+featureDefn = layer1.GetLayerDefn()
 
 with gzip.open('index.csv.gz', 'rt') as f:
     line = f.readline() #skip the headers
-    for i in range(10):
+    layer1.StartTransaction()
+    for i in itertools.count():
         line = f.readline()
+        if not line:
+            break
         #print(line)
-        print(parseRecord(line))
+        #print(parseRecord(line))
+        granule=parseRecord(line)
+
+        if (i % 10000 == 0 and i > 0):
+            print(i)
+            #layer1.CommitTransaction()
+            #layer1.StartTransaction()
+
+        ring = ogr.Geometry(ogr.wkbLinearRing)
+        ring.AddPoint(granule.WEST_LON, granule.SOUTH_LAT)
+        ring.AddPoint(granule.EAST_LON, granule.SOUTH_LAT)
+        ring.AddPoint(granule.EAST_LON, granule.NORTH_LAT)
+        ring.AddPoint(granule.WEST_LON, granule.NORTH_LAT)
+        ring.AddPoint(granule.WEST_LON, granule.SOUTH_LAT)
+        poly = ogr.Geometry(ogr.wkbPolygon)
+        poly.AddGeometry(ring)
+
+        feature=ogr.Feature(featureDefn)
+        feature.SetGeometry(poly)
+        feature.SetField("granule_id", granule.GRANULE_ID)
+        feature.SetField("product_id", granule.PRODUCT_ID)
+        feature.SetField("cloud_cover", granule.CLOUD_COVER)
+        feature.SetField("sensing_date", granule.SENSING_TIME.strftime(date_format))
+        feature.SetField("mgrs_tile", granule.MGRS_TILE)
+        layer1.CreateFeature(feature)
+        feature=None
+
+print("Commit transaction...")
+layer1.CommitTransaction()
+print("Done.")
+layer1 = None
+dataset = None
+
 
---
+++ sentinelgcp.py
@@ -0,0 +1,51 @@
+from collections import namedtuple
+from datetime import datetime
+
+GcpAddress = namedtuple('GcpAddress', ['bucket', 'folder', 'image_format'])
+SENTINEL_BUCKET_NAME="gcp-public-data-sentinel-2"
+
+def GetGcpAddress(mgrstile:str, productId:str, granuleId:str) -> GcpAddress:
+    if granuleId.startswith("S2A_OPER_"):
+        return GetGcpAddressOld(mgrstile, productId, granuleId)
+    elif granuleId.startswith("L1C_"):
+        return GetGcpAddressNew(mgrstile, productId, granuleId)
+    else:
+        return GetGcpAddressFullSearch(mgrstile, productId, granuleId)
+
+def GetGcpAddressOld(mgrstile:str, productId:str, granuleId:str) -> GcpAddress:
+    addr=GcpAddress(SENTINEL_BUCKET_NAME,    
+    "tiles/{t1}/{t2}/{t3}/{product}/GRANULE/{granule}/IMG_DATA".format(
+        t1=mgrstile[0:2], 
+        t2=mgrstile[2:3], 
+        t3=mgrstile[3:],
+        product=productId,
+        granule=granuleId),
+        "_".join(granuleId.split('_')[:10])+"_B{0:02d}.jp2"
+    )
+    return addr
+
+def GetGcpAddressNew(mgrstile:str, productId:str, granuleId:str) -> GcpAddress:
+    addr=GcpAddress(SENTINEL_BUCKET_NAME,    
+    "tiles/{t1}/{t2}/{t3}/{product}/GRANULE/{granule}/IMG_DATA".format(
+        t1=mgrstile[0:2], 
+        t2=mgrstile[2:3], 
+        t3=mgrstile[3:],
+        product=productId,
+        granule=granuleId),
+        "T"+"_".join([mgrstile, *productId.split('_')[2:3]])+"_B{0:02d}.jp2"
+    )
+    return addr
+
+def GetGcpAddressFullSearch(mgrstile:str, productId:str, granuleId:str) -> GcpAddress:
+    pass
+
+if __name__=="__main__":
+    #mgrsTile="14SPJ"
+    #productId="S2A_MSIL1C_20151001T173006_N0204_R012_T14SPJ_20161104T032429.SAFE"
+    #granuleId="S2A_OPER_MSI_L1C_TL_EPA__20161104T015254_A001439_T14SPJ_N02.04"
+    mgrsTile="14SPJ"
+    productId="S2A_MSIL1C_20170520T171301_N0205_R112_T14SPJ_20170520T172251.SAFE"
+    granuleId="L1C_T14SPJ_A009976_20170520T172251"
+    addr=GetGcpAddress(mgrsTile, productId, granuleId)
+    print(addr)
+    print(addr.image_format.format(8))
